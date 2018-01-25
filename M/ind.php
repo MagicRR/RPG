39f08faad83f9205ee6bec37628edb9d16342fb6
MagicRR <r.richard.94@orange.fr>
2018-01-25 12:21:47 +0100

    test stash

---
+++ M/ind.php
@@ -0,0 +1,17 @@
+<?php include("../model/Personnage.class.php");?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Personnage</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+
+        <?php echo "Bonjour Stash"; ?>
+
+    </body>
+
+</html>
